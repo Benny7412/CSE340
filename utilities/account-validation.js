e5df4f8f693bac18a349c8a0ce23d8c445c8f578
Benny7412 <bennyklaus@gmail.com>
2025-06-14 18:45:08 -0500

    cleaning up code

--- utilities/account-validation.js
+++ utilities/account-validation.js
@@ -7,48 +7,20 @@ const utilities = require(".")
   * ********************************* */
   validate.registationRules = () => {
     return [
-      // firstname is required and must be string
-      body("account_firstname")
-        .trim()
-        .escape()
-        .notEmpty()
-        .isLength({ min: 1 })
-        .withMessage("Please provide a first name."), // on error this message is sent.
-  
-      // lastname is required and must be string
-      body("account_lastname")
-        .trim()
-        .escape()
-        .notEmpty()
-        .isLength({ min: 2 })
-        .withMessage("Please provide a last name."), // on error this message is sent.
-  
-      // valid email is required and cannot already exist in the DB
-      body("account_email")
-      .trim()
-      .escape()
-      .notEmpty()
-      .isEmail()
-      .normalizeEmail() // refer to validator.js docs
-      .withMessage("A valid email is required."),
-  
-      // password is required and must be strong password
-      body("account_password")
-        .trim()
-        .notEmpty()
-        .isStrongPassword({
-          minLength: 12,
-          minLowercase: 1,
-          minUppercase: 1,
-          minNumbers: 1,
-          minSymbols: 1,
-        })
-        .withMessage("Password does not meet requirements."),
+      body("account_firstname").trim().escape().notEmpty().isLength({ min: 1 }).withMessage("Please provide a first name."),
+      body("account_lastname").trim().escape().notEmpty().isLength({ min: 2 }).withMessage("Please provide a last name."),
+      body("account_email").trim().escape().notEmpty().isEmail().normalizeEmail().withMessage("A valid email is required."),
+      body("account_password").trim().notEmpty().isStrongPassword({
+minLength: 12,
+minLowercase: 1,
+minUppercase: 1,
+minNumbers: 1,
+minSymbols: 1,}).withMessage("Password does not meet requirements."),
     ]
   }
 
   /* ******************************
- * Check data and return errors or continue to registration
+ * Validate data
  * ***************************** */
 validate.checkRegData = async (req, res, next) => {
   const { account_firstname, account_lastname, account_email } = req.body
@@ -65,8 +37,7 @@ validate.checkRegData = async (req, res, next) => {
       account_email,
     })
     return
-  }
-  next()
+  } next()
 }
 
 module.exports = validate
--- utilities/pickInventory.js
+++ utilities/pickInventory.js
@@ -1,5 +1,6 @@
 const FIELDS = require('../constants/inventoryFields');
 
+//spreads inv into require fields
 module.exports = function pickInventory(source) {
   return FIELDS.reduce((obj, key) => {
     if (key in source) obj[key] = source[key];
